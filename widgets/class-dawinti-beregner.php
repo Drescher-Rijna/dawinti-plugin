d4ef2fa5c34931026560b95e70f7b5e70651cbef
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-05-28 22:28:30 +0200

    font sizing

--- widgets/class-dawinti-beregner.php
+++ widgets/class-dawinti-beregner.php
@@ -35,7 +35,7 @@ class Dawinti_Beregner extends Widget_Base {
 	public function __construct( $data = array(), $args = null ) {
 		parent::__construct( $data, $args );
 
-		wp_register_style( 'beregnercss', plugins_url( '/assets/css/beregner.css', ELEMENTOR_DAWINTI ), array(), '1.0.0' );
+		wp_register_style( 'beregnercss', plugins_url( '/assets/css/beregner.css', ELEMENTOR_DAWINTI ), array(), 'all' );
         wp_register_script( 'beregnerjs', plugins_url( '/assets/js/beregner.js', ELEMENTOR_DAWINTI ), array(), '1.0.0' );
 
 		
